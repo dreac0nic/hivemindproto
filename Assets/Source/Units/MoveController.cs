aa59dab8d8eb642a1ed287ec6dba5d7e0e6dd90c
leyarotheconquerer <HazenJohnson@letu.edu>
2015-01-27 02:39:08 -0600

    Add generic unit with basic animations.

---
+++ Assets/Source/Units/MoveController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public bool walking = false;
+
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	void Update () {
+		animator.SetBool("Walk", walking);
+	}
+}
