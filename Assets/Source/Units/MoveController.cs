bac93a209b1eb175e765752598568b493b4e9a32
leyarotheconquerer <HazenJohnson@letu.edu>
2015-01-28 02:44:03 -0600

    Add NavMeshAgent component to the generic unit.
    Also, prototype how we could do movement. Now we just need to code it nicely.

--- Assets/Source/Units/MoveController.cs
+++ Assets/Source/Units/MoveController.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class MoveController : MonoBehaviour {
 
 	public bool walking = false;
+	public Vector3 destination = Vector3.zero;
 
 	private Animator animator;
+	private NavMeshAgent navAgent;
 
 	void Start () {
 		animator = GetComponent<Animator>();
+		navAgent = GetComponent<NavMeshAgent>();
 	}
 
 	void Update () {
 		animator.SetBool("Walk", walking);
+		navAgent.SetDestination(destination);
 	}
 }
