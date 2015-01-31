8ce19f8621397e57d46f2ed7da6c7c1e880e8ed5
drac0nic <spenser.m.bray@gmail.com>
2015-01-31 16:46:36 -0600

    Modify HUD code to test selecting behaviors.

--- Assets/Source/Player/HUD.cs
+++ Assets/Source/Player/HUD.cs
@@ -19,7 +19,19 @@ public class HUD : MonoBehaviour
 	{
 		GUI.skin = resourceSkin;
 		GUI.BeginGroup(new Rect(Screen.width/2 - RESOURCE_BAR_WIDTH/2, 0, RESOURCE_BAR_WIDTH, RESOURCE_BAR_HEIGHT));
-		GUI.Label(new Rect(0, 0, RESOURCE_BAR_WIDTH, RESOURCE_BAR_HEIGHT), "RESOURCES");
+
+		Selector compie = player.GetComponent<Selector>();
+		if(compie && compie.SelectedUnits.Count > 0) {
+			StringBuilder buffer = new StringBuilder();
+
+			//Iterate over list and add each name for selected items... when it exists.
+
+			buffer.Append(compie.SelectedUnits.Count);
+
+			GUI.Label(new Rect(0, 0, RESOURCE_BARD_WIDTH, RESOURCE_BAR_HEIGHT), buffer.toString());
+		} else
+			GUI.Label(new Rect(0, 0, RESOURCE_BAR_WIDTH, RESOURCE_BAR_HEIGHT), "RESOURCES");
+
 		GUI.EndGroup();
 	}
 }
