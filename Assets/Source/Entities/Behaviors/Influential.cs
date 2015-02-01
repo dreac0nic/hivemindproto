6050f7f8a1b5e1f8e678213add5f56db1caedcf5
drac0nic <spenser.m.bray@gmail.com>
2015-02-01 15:15:55 -0600

    Basic Influence framework setup.

--- Assets/Source/Entities/Behaviors/Influential.cs
+++ Assets/Source/Entities/Behaviors/Influential.cs
@@ -17,4 +17,12 @@ public class Influential : MonoBehaviour
 	{
 
 	}
+
+	// Returns the power at the given point of the influence bubble.
+	public float Power(Vector3 pos)
+	{
+		float distance = Vector3.Distance(this.transform.root.position, pos);
+
+		return -distance+100;
+	}
 }
--- Assets/Source/Entities/Behaviors/Loyal.cs
+++ Assets/Source/Entities/Behaviors/Loyal.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class Loyal : MonoBehaviour
 {
 	public Influential allegiance;
-	public double loyalty = 0;
+	public float loyalty = 0;
+	public int duration = 0;
 
 	void Start()
 	{
@@ -13,6 +14,43 @@ public class Loyal : MonoBehaviour
 
 	void Update()
 	{
+		if(allegiance) {
+			// Update loyalty.
+			loyalty = TimeModifier()*allegiance.Power(transform.root.position);
 
+			if(loyalty < 0) {
+				// Check for other queens.
+				Influential[] queens = (Influential[])this.transform.root.GetComponentsInParent(typeof(Influential));
+
+				if(queens.Length >= 1) {
+					// Reassign to new queen.
+					Influential newQueen = null;
+					float power = 0f;
+
+					foreach(Influential heir in queens) {
+						float tempPower = heir.Power(transform.root.position);
+
+						if(tempPower > power) {
+							newQueen = heir;
+							power = tempPower;
+						}
+					}
+					
+					allegiance = newQueen;
+				} else {
+					allegiance = null;
+				}
+			}
+		} else {
+			// Feral!
+		}
+	}
+
+	protected float TimeModifier()
+	{
+		float value = 0.5f*duration*duration;
+
+		if(value > 1.0) return 1.0f;
+		else return value;
 	}
 }
