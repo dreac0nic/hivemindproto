914ef6d4121e2a9b2fce682d0e91b65eba756dfd
drac0nic <spenser.m.bray@gmail.com>
2015-02-01 20:20:30 -0600

    It helps to have the files that are being worked on and whatever.

---
+++ Assets/Source/Entities/Behaviors/Movable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movable : MonoBehaviour
+{
+	public Vector3 destination = Vector3.zero;
+
+	private Animator animator;
+	private NavMeshAgent navAgent;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		navAgent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+	{
+		navAgent.SetDestination(destination);
+	}
+}
