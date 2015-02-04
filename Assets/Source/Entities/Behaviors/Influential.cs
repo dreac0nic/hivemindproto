2c6bcf290b87bf945736d818f4054fb95b3ccbcf
dreac0nic <matthewqa@gmail.com>
2015-02-04 16:06:10 -0600

    Update influence bubbles to properly instantiate and attach from behavior.

--- Assets/Source/Entities/Behaviors/Influential.cs
+++ Assets/Source/Entities/Behaviors/Influential.cs
@@ -1,24 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Influential : MonoBehaviour
 {
 	/* A single "strength" modifier. This encompasses both the power and distance of transmission.
-	 * For now, merely specifies distance, but later the algorithm should be tweaked appropriately.
-	 */
+	* For now, merely specifies distance, but later the algorithm should be tweaked appropriately.
+	*/
 	public float strength = 100;
+
+	private GameObject influenceBubble = null;
 
 	void Start()
 	{
+		influenceBubble = (GameObject)Instantiate(Resources.Load("InfluenceBubble"));
 
+		influenceBubble.transform.parent = transform;
+
+		influenceBubble.transform.localPosition = new Vector3(0f, 0f, 0f);
 	}
 
 	void Update()
 	{
-		var influenceSphere = transform.parent.Find("influenceBubble");
-
-		if(influenceSphere)
-			influenceSphere.transform.localScale = 2*(new Vector3(strength, strength, strength));
+		if(influenceBubble)
+			influenceBubble.transform.localScale = 2*(new Vector3(strength, strength, strength));
 	}
 
 	// Returns the power at the given point of the influence bubble.
@@ -29,4 +33,3 @@ public class Influential : MonoBehaviour
 		return -distance + strength;
 	}
 }
-
