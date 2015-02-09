5ddc91e641f730062afa62ae643959c816ca3af5
leyarotheconquerer <HazenJohnson@letu.edu>
2015-02-09 00:07:37 -0600

    Add graphical unit selection.

--- Assets/Source/Entities/Behaviors/Selectable.cs
+++ Assets/Source/Entities/Behaviors/Selectable.cs
@@ -3,17 +3,42 @@ using System.Collections;
 
 public class Selectable : MonoBehaviour
 {
-	public bool Selected { get { return currentlySelected; } set { currentlySelected = value; } }
+	public GameObject SelectMarker;
+	public bool Selected {
+		get {
+			return currentlySelected;
+		}
+		set {
+			if(value && !currentlySelected) { addSelectMarker(); }
+			else if(currentlySelected) { removeSelectMarker(); }
+			currentlySelected = value;
+		}
+	}
 
 	protected bool currentlySelected;
+	private GameObject selectMarker;
 
 	void Start()
 	{
-
 	}
 
 	void Update()
 	{
 
+	}
+
+	void addSelectMarker()
+	{
+		selectMarker = (GameObject)Instantiate(SelectMarker, Vector3.zero, Quaternion.identity);
+		selectMarker.transform.SetParent(transform, false);
+
+		// Hard-coded rotation of the select marker
+		// TODO: Figure out how to encode this in the select marker prefab
+		selectMarker.transform.Rotate(new Vector3(90, 0, 0));
+	}
+
+	void removeSelectMarker()
+	{
+		Destroy(selectMarker);
 	}
 }
