79538175cf5f0c2a32a78cd084d6929c196f8234
dreac0nic <matthewqa@gmail.com>
2015-02-11 16:02:14 -0600

    Give Commandable some basic code.

--- Assets/Source/Entities/Behaviors/Commandable.cs
+++ Assets/Source/Entities/Behaviors/Commandable.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
+using System.Tuple;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Commandable : MonoBehaviour
 {
+    public Queue< Tuple<string, string> > orders;
+    
     void Start() {
-	
+	orders = new Queue< Tuple<string, string> >;
     }
     
     void Update() {
-	
+	Debug.log("I AM DOING " + orders.peek().Item1 + " AT " + orders.peek().Item2);
+    }
+
+    public void order(string order, string data, bool clearOrders = true) {
+	if(clearOrders)
+	    orders.Clear();
+
+	orders.Enqueue(new Tuple<string, string>(order, data));
     }
 }
