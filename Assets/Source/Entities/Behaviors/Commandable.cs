5edfb5432d2724af4a28ae907896a4742f54c26c
leyarotheconquerer <HazenJohnson@letu.edu>
2015-02-11 22:04:33 -0600

    Add placeholder bug fixes.
    Unfortunately, the Unity Mono compiler only supports up through .NET 4, so we don't have Tuples yet. Or maybe it just doesn't support that part of .NET. Either way, we don't have Tuples. Consider using something else?

--- Assets/Source/Entities/Behaviors/Commandable.cs
+++ Assets/Source/Entities/Behaviors/Commandable.cs
@@ -1,24 +1,34 @@
-﻿using UnityEngine;
-using System.Tuple;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuple<T1, T2>
+{
+	public T1 First { get; private set; }
+	public T2 Second { get; private set; }
+	internal Tuple(T1 first, T2 second)
+	{
+		First = first;
+		Second = second;
+	}
+}
 
 public class Commandable : MonoBehaviour
 {
     public Queue< Tuple<string, string> > orders;
     
     void Start() {
-	orders = new Queue< Tuple<string, string> >;
+		orders = new Queue< Tuple<string, string> >();
     }
     
     void Update() {
-	Debug.log("I AM DOING " + orders.peek().Item1 + " AT " + orders.peek().Item2);
+		Debug.Log("I AM DOING " + orders.Peek().First + " AT " + orders.Peek().Second);
     }
 
     public void order(string order, string data, bool clearOrders = true) {
-	if(clearOrders)
-	    orders.Clear();
+		if(clearOrders)
+		    orders.Clear();
 
-	orders.Enqueue(new Tuple<string, string>(order, data));
+		orders.Enqueue(new Tuple<string, string>(order, data));
     }
 }
