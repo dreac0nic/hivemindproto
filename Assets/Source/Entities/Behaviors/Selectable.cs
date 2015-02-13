efa0b39ade3dd1ca8a3954067b25216092e2e1b3
leyarotheconquerer <HazenJohnson@letu.edu>
2015-02-13 02:47:09 -0600

    Make HP bars show up conditionally when clicked.

--- Assets/Source/Entities/Behaviors/Selectable.cs
+++ Assets/Source/Entities/Behaviors/Selectable.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using HiveMind;
+
 public class Selectable : MonoBehaviour
 {
-	public GameObject SelectMarker;
 	public bool Selected {
 		get {
 			return currentlySelected;
@@ -17,20 +18,32 @@ public class Selectable : MonoBehaviour
 
 	protected bool currentlySelected;
 	private GameObject selectMarker;
+	private GameObject unitUi;
+	private RectTransform hpBarRatio;
+	private UnitStats unitStats;
 
 	void Start()
 	{
+		unitStats = GetComponent<UnitStats>();
 	}
 
 	void Update()
 	{
-
+		if(unitUi && hpBarRatio)
+		{
+			hpBarRatio.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 512f * (float)unitStats.Health / (float)unitStats.MaxHealth);
+			unitUi.transform.LookAt(new Vector3(transform.position.x, Options.O.UICamera.transform.position.y, transform.position.z));
+		}
 	}
 
 	void addSelectMarker()
 	{
-		selectMarker = (GameObject)Instantiate(SelectMarker, Vector3.zero, Quaternion.identity);
+		selectMarker = (GameObject)Instantiate(Options.O.SelectMarker, Vector3.zero, Quaternion.identity);
 		selectMarker.transform.SetParent(transform, false);
+
+		unitUi = (GameObject)Instantiate(Resources.Load("UnitUI"), Vector3.up * 7, Quaternion.identity);
+		unitUi.transform.SetParent(transform, false);
+		hpBarRatio = unitUi.transform.Find("HpRatio").gameObject.GetComponent<RectTransform>();
 
 		// Hard-coded rotation of the select marker
 		// TODO: Figure out how to encode this in the select marker prefab
@@ -40,5 +53,7 @@ public class Selectable : MonoBehaviour
 	void removeSelectMarker()
 	{
 		Destroy(selectMarker);
+		Destroy(unitUi);
+		hpBarRatio = null;
 	}
 }
